cb98f87f2f8a90cb6be2c261dbd84dfe216b7cbe
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2021-01-12 00:46:04 -0600

    adding starting files

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <h1 className="display-4">Employee Directory</h1>
+            <p className="lead">Search an employees name in the employee directory</p>
+        </div>
+
+
+    )
+}
+
+export default Jumbotron;
